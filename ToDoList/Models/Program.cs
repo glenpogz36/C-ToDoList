233e216b051ea6f27b506233f4beefac1bf92d64
Glen Sale and James Cho <glen@>
2018-11-15 12:44:20 -0800

    it will initialize add but stops at view

--- ToDoList/Models/Program.cs
+++ ToDoList/Models/Program.cs
@@ -3,67 +3,88 @@ using System.Collections.Generic;
 using ToDoList.Models;
 
 namespace ToDoList.Models
-{
+{  
    public class Program 
    {
+         public class Item
+        {
+            private string _description;
+            private static List<Item> _instances = new List<Item> {};
+
+            public Item (string description)
+            {
+            _description = description;
+            _instances.Add(this);
+            }
+
+            public string GetDescription()
+            {
+            return _description;
+            }
+
+            public void SetDescription(string newDescription)
+            {
+                _description = newDescription;
+            }
+
+            public static List<Item> GetAll()
+            {
+            return _instances;
+            }
+
+            public static void ClearAll()
+            {
+                _instances.Clear();
+            }
+        }
+
+
         public static void Main(string[] args) 
         {
-            List<string> ToDoInput = new List<string>();
-
-            Console.WriteLine ("Welcome to the To Do List");
-            Console.WriteLine("Would you like to add an item to your list or view your list? (Add/View)");
-
-            string answer = Console.ReadLine();
-            if(answer == "Add")
-           {    
-                addItems();
-           }
-           else if (answer =="View" && ToDoInput.Count != 0)
-           {
-                viewItems();
-           } 
-           else if (answer =="View" && ToDoInput.Count == 0)
-           {
-                Console.WriteLine("Your To Do List is empty at the moment.");
-           }         
-
-            public static void addItems()
-        {  
-                Console.WriteLine ("Please enter the desciption of your item to add.");
-                string description = Console.ReadLine();
-            
-            while (!string.IsNullOrEmpty(description))
-            {
-                ToDoInput.Add(description);
-                Console.WriteLine("Would you like to Add or View? (Add/View)");
-                answer = Console.ReadLine();
-
-                if(answer == "View")
-                {
-                viewItems();
-                }
-                else if(answer =="Add")
-                {
-                 addItems();
-                }
-                
-            }
-         }
-
-        public static string viewItems()
-         {
-                 Console.WriteLine("Here are your items.");
-                 foreach (string item in ToDoInput)
-                {
-                Console.WriteLine(item);
-                }
-         }
-         
-       
-                
-      
-         
-
-        }
+             
+          Console.WriteLine ("Welcome to the To Do List");
+           List<string> newList = new List<string>();
            
+          Console.WriteLine("Would you like to Add, View or Exit? (Add/View/Exit)");
+          string option = Console.ReadLine();   
+           if(!string.IsNullOrEmpty(option))
+          {
+               while(option =="Add")
+          {
+          Console.WriteLine ("Please enter the desciption of your item to add.");
+          string description = Console.ReadLine();
+          
+          
+          Item newItem = new Item(description);
+          string thing = newItem.GetDescription();
+          newList.Add(thing);
+          
+
+          Console.WriteLine("Add, View or Exit? (Add/View/Exit)");
+          option = Console.ReadLine(); 
+          }
+
+          while(option=="View")
+          {
+           Console.WriteLine("Here are the items in your list."); 
+          foreach(object item in newList)
+          {
+           Console.WriteLine(item);
+
+          }
+         Console.WriteLine("Add, View or Exit? (Add/View/Exit)");
+          option = Console.ReadLine(); 
+          
+          }
+
+          if(option=="Exit")
+          {
+               return;
+          }
+         
+         }
+         
+     }
+              
+}        
 }
